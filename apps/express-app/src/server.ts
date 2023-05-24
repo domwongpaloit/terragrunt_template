8f586c7dc7fdbe00eb2c788c0a06d2112db2d823
Dom Wong <dwong@palo-it.com>
2023-05-24 14:29:07 +0800

    example for accessing public dockerhub

--- apps/express-app/src/server.ts
+++ apps/express-app/src/server.ts
@@ -3,5 +3,5 @@ import app from "./app";
 const port = 3000;
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+    console.log(`Server running on port ${port} v3.1.2`)
 })
