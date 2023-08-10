1e7173bf7f98c778ca24eed355b17eee703474f9
Dom Wong <dwong@palo-it.com>
2023-08-10 08:57:56 +0800

    kerpenter basic and argocd rollout sh

--- infrastructure-live/terragrunt.hcl
+++ infrastructure-live/terragrunt.hcl
@@ -36,7 +36,7 @@ terraform {
     }
     aws = {
       source = "hashicorp/aws"
-      version = "4.67.0"
+      version = "5.11.0"
     }
   }
 
--- terraform-modules/eks/eks.tf
+++ terraform-modules/eks/eks.tf
@@ -22,3 +22,32 @@ module "eks" {
     Terraform   = true
   }
 }
+
+module "eks_blueprints_addons" {
+  source  = "aws-ia/eks-blueprints-addons/aws"
+  version = "1.5.0" #ensure to update this to the latest/desired version
+
+  cluster_name      = module.eks.cluster_name
+  cluster_endpoint  = module.eks.cluster_endpoint
+  cluster_version   = module.eks.cluster_version
+  oidc_provider_arn = module.eks.oidc_provider_arn
+
+  enable_karpenter = true
+
+  tags = {
+    Environment = var.env,
+    Terraform   = true
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+      command     = "aws"
+    }
+  }
+}
